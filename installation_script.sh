9a2ca91ffc9d0b6b12742040329f890d5d67dc0b
omkargudekar <omkargudekar@gmail.com>
2015-11-28 20:05:14 -0800

    updated

--- installation_script.sh
+++ installation_script.sh
@@ -2,9 +2,10 @@
 setterm -term linux -back black -fore green -clear
 
 curl https://raw.githubusercontent.com/omkargudekar/pagemetrix-info/master/credit.txt | cat
-
+wait
 sleep 3
 echo "Installation started....\n"
+wait
 sleep 3
 
 read -p "Please select the port for node-server: " nodeport
@@ -13,20 +14,20 @@ read -p "Please select the port for redi-server: " redisport
 
 
 echo "Checking for git installation..."
-
+wait
 if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "No git found. Setting up wget."
   apt-get --force-yes --yes install git
 fi
-
+wait
 
 if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "No nodejs found. Setting up wget."
   apt-get --force-yes --yes install wget
 fi
-
+wait
 echo "Checking for nodejs installation..."
 
 if [ $(dpkg-query -W -f='${Status}' nodejs 2>/dev/null | grep -c "ok installed") -eq 0 ];
@@ -34,7 +35,7 @@ then
   echo "No nodejs found. Setting up nodejs."
   apt-get --force-yes --yes install nodejs
 fi
-
+wait
 echo "Checking for npm installation..."
 
 if [ $(dpkg-query -W -f='${Status}' nodejs npm 2>/dev/null | grep -c "ok installed") -eq 0 ];
@@ -42,14 +43,14 @@ then
   echo "No nodejs found. Setting up npm."
   apt-get --force-yes --yes install nodejs npm
 fi
-
+wait
 echo "Checking for redis-server installation..."
-
 if [ $(dpkg-query -W -f='${Status}' redis-server 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "No nodejs found. Setting up redis-server."
   apt-get --force-yes --yes install redis-server
 fi
+wait
 
 echo "Checking for unzip installation..."
 
@@ -58,14 +59,20 @@ then
   echo "No unzip found. Setting up unzip."
   apt-get --force-yes --yes install unzip
 fi
+wait
 
 git clone https://github.com/omkargudekar/pagemetrics.git
+wait
 cd pagemetrics
 npm install
+wait
 npm install nodemon -g
-
+wait
 
 apt-get install redis-server
+wait
+
+
 nohup redis-server --port $redisport >redis.log &
 echo $$ > redit_agent_pid
 nohup nodemon --port $nodeport > dashboard.log &
@@ -83,7 +90,7 @@ then
 else
     echo "This URL Not Exist"
 fi
-
+wait
 
 echo "Bye Bye.. Installation complete...nJoy :) "
 
