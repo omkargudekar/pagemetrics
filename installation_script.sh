8e1e9089060ee7d8edd9b4c66beacebfd3c99642
omkargudekar <omkargudekar@gmail.com>
2015-11-28 19:26:25 -0800

    added-steps

---
+++ installation_script.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+setterm -term linux -back black -fore green -clear
+
+cat credit.txt
+echo "################################################################"
+echo "Installation"
+echo "################################################################"
+
+read -p "Please select the port where you want to run the dashboard : " nodeport
+read -p "Please select the p: " redisport
+
+
+
+echo "Checking for wget installation..."
+
+if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "No nodejs found. Setting up wget."
+  apt-get --force-yes --yes install wget
+fi
+
+echo "Checking for nodejs installation..."
+
+if [ $(dpkg-query -W -f='${Status}' nodejs 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "No nodejs found. Setting up nodejs."
+  apt-get --force-yes --yes install nodejs
+fi
+
+echo "Checking for npm installation..."
+
+if [ $(dpkg-query -W -f='${Status}' nodejs npm 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "No nodejs found. Setting up npm."
+  apt-get --force-yes --yes install nodejs npm
+fi
+
+echo "Checking for redis-server installation..."
+
+if [ $(dpkg-query -W -f='${Status}' redis-server 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "No nodejs found. Setting up redis-server."
+  apt-get --force-yes --yes install redis-server
+fi
+
+
+
+wget https://github.com/omkargudekar/pagemetrix-info/pagemetrics-dashboard.tar.bz2
+tar -jxvf pagemetrix-dashboard.tar.bz2
+cd pagemetrix-dashboard
+npm install
+npm install nodemon -g
+
+
+apt-get install redis-server
+nohup redis-server --port $redisport &
+echo $$ > agent_pid
+nohup nodemon --port $nodeport &
+echo $$ > agent_pid
+cd scripts
+sh runagent.sh
+echo $$ > agent_pid
+
+echo "Bye Bye.. Installation complete...nJoy :) "
+
+
+
+
+
+
