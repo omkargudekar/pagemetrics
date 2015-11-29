6deefeafb9ae677def70007e4c730c26c4ea5210
omkargudekar <omkargudekar@gmail.com>
2015-11-28 21:13:09 -0800

    updated

--- installation_script.sh
+++ installation_script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 setterm -term linux -back black -fore green -clear
+$nodeport=52300
 
 curl https://raw.githubusercontent.com/omkargudekar/pagemetrix-info/master/credit.txt | cat
 wait
@@ -13,59 +14,58 @@ echo "Checking for git installation..."
 wait
 if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  echo "No git found. Setting up wget."
+  echo "No git found. Setting up git."
   apt-get --force-yes --yes install git
+else
+  echo "git is already installed..."
 fi
 wait
 
 if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  echo "No nodejs found. Setting up wget."
+  echo "No wget found. Setting up wget."
   apt-get --force-yes --yes install wget
+else
+  echo "wget is already installed..."
 fi
 wait
+
 echo "Checking for nodejs installation..."
 
 if [ $(dpkg-query -W -f='${Status}' nodejs 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "No nodejs found. Setting up nodejs."
   apt-get --force-yes --yes install nodejs
+  else
+  echo "nodejs is already installed..."
 fi
 wait
 echo "Checking for npm installation..."
 
 if [ $(dpkg-query -W -f='${Status}' nodejs npm 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  echo "No nodejs found. Setting up npm."
+  echo "No npm found. Setting up npm."
   apt-get --force-yes --yes install nodejs npm
+  else
+  echo "npm is already installed..."
 fi
 wait
 echo "Checking for redis-server installation..."
 if [ $(dpkg-query -W -f='${Status}' redis-server 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  echo "No nodejs found. Setting up redis-server."
+  echo "No redis-server found. Setting up redis-server."
   apt-get --force-yes --yes install redis-server
+  else
+  echo "redis-server is already installed..."
 fi
 wait
 
-echo "Checking for unzip installation..."
 
-if [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "No unzip found. Setting up unzip."
-  apt-get --force-yes --yes install unzip
-fi
-wait
-
+echo "Downloading code from repository..."
 git clone https://github.com/omkargudekar/pagemetrics.git
 wait
-cd pagemetrics
-npm install
-wait
-npm install nodemon -g
-wait
 
-
+echo "Cleaning previous processes...if any..."
 pkill -f wrapper_dashboard_998899.sh
 wait
 rm -rf pagemetrics
@@ -73,26 +73,54 @@ wait
 
 
 
+echo "Cleaning previous installations..if any..."
+cd pagemetrics
+npm install
+wait
+npm install nodemon -g
+wait
+
+
+echo "Running pagemetrics agent..."
 cd scripts
 nohup sh runagent.sh &
 echo $$ > pagemetrics_agent_pid
 
 
-echo -n "Please select the port for node-server: "
-read -p nodeport
+echo "Checking redis server..."
+
+redisReply= `redis-cli PING`
+
+if [ $redisReply -ne "PONG" ];
+then
+  echo "Redis server is down. Starting redis-server"
+  echo -n "Please select the port for redis-server: "
+  read -p redisport
+  wait
+  nohup redis-server >redis.log &
+  echo $$ > redit_agent_pid
+else
+
+
+
+echo "Starting node server..."
+
+
 nohup nodemon --port $nodeport > dashboard.log &
 echo $$ > nodeserver_pid
 
-echo -n "Please select the port for redis-server: "
-read -p redisport
-wait
-nohup redis-server --port $redisport >redis.log &
-echo $$ > redit_agent_pid
+
+
+  echo "Redis-Server is already running..."
+fi
+
 
 
 
 publicIP= `wget http://ipinfo.io/ip -qO -`
 dashboardURL=publicIP':'$nodeport
+
+
 
 if curl --output /dev/null --silent --head --fail "$dashboardURL"
 then
