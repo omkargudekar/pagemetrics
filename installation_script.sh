23156514c5484f6ad3a258c5659313782b0fa798
omkargudekar <omkargudekar@gmail.com>
2015-11-28 19:58:49 -0800

    updated installation script

--- installation_script.sh
+++ installation_script.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 setterm -term linux -back black -fore green -clear
 
-cat credit.txt
-echo "Installation\n"
-echo "################################################################\n"
+curl https://raw.githubusercontent.com/omkargudekar/pagemetrix-info/master/credit.txt | cat
+
+echo "Installation started....\n"
 
 read -p "Please select the port for node-server: " nodeport
 read -p "Please select the port for redi-server: " redisport
 
 
 
-echo "Checking for wget installation..."
+echo "Checking for git installation..."
+
+if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "No git found. Setting up wget."
+  apt-get --force-yes --yes install git
+fi
+
 
 if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -42,22 +49,27 @@ then
   apt-get --force-yes --yes install redis-server
 fi
 
+echo "Checking for unzip installation..."
 
+if [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "No unzip found. Setting up unzip."
+  apt-get --force-yes --yes install unzip
+fi
 
-wget https://github.com/omkargudekar/pagemetrix-info/blob/master/pagemetrics-dashboard.tar.bz2?raw=true
-tar -jxvf pagemetrix-dashboard.tar.bz2
-cd pagemetrix-dashboard
+git clone https://github.com/omkargudekar/pagemetrics.git
+cd pagemetrics
 npm install
 npm install nodemon -g
 
 
 apt-get install redis-server
-nohup redis-server --port $redisport &
+nohup redis-server --port $redisport >redis.log &
 echo $$ > redit_agent_pid
-nohup nodemon --port $nodeport &
+nohup nodemon --port $nodeport > dashboard.log &
 echo $$ > nodeserver_pid
 cd scripts
-sh runagent.sh
+nohup sh runagent.sh &
 echo $$ > pagemetrics_agent_pid
 
 echo "Bye Bye.. Installation complete...nJoy :) "
