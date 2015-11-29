b9b23a46a3d9aeb487c0eaf1db3245c3c7dc7f76
omkargudekar <omkargudekar@gmail.com>
2015-11-29 03:38:54 -0800

    updated

--- installation_script.sh
+++ installation_script.sh
@@ -8,68 +8,45 @@ echo "Installation started...."
 wait
 sleep 3
 
+echo "updating apt-get list..."
 
-echo "Checking for git installation..."
+apt-get update --force-yes --yes &
 wait
-if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "No git found. Setting up git."
-  apt-get --force-yes --yes install git &
-  wait
-else
-  echo "git is already installed..."
-fi
+
+echo "Checking git.."
+
+apt-get --force-yes --yes install git &
+wait
+
+echo "Checking wget.."
+
+apt-get --force-yes --yes install wget &
+wait
 
 
-if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "No wget found. Setting up wget."
-  apt-get --force-yes --yes install wget &
-  wait
-else
-  echo "wget is already installed..."
-fi
+echo "Checking for nodejs..."
+apt-get --force-yes --yes install nodejs &
+wait
 
-echo "Checking for nodejs installation..."
-
-if [ $(dpkg-query -W -f='${Status}' nodejs 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "No nodejs found. Setting up nodejs."
-  apt-get --force-yes --yes install nodejs &
-  wait
-  else
-  echo "nodejs is already installed..."
-fi
+echo "creating node alias"
 
 ln -s `which nodejs` /usr/bin/node
 
-echo "Checking for npm installation..."
 
-if [ $(dpkg-query -W -f='${Status}' nodejs npm 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "No npm found. Setting up npm."
-  apt-get --force-yes --yes install npm &
-  wait
-  else
-  echo "npm is already installed..."
-fi
+echo "Checking for npm..."
+apt-get --force-yes --yes install npm &
+wait
 
-echo "Checking for redis-server installation..."
-if [ $(dpkg-query -W -f='${Status}' redis-server 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "No redis-server found. Setting up redis-server."
-  apt-get --force-yes --yes install redis-server &
-  wait
-  else
-  echo "redis-server is already installed..."
-fi
+
+echo "Checking for redis-server ..."
+apt-get --force-yes --yes install redis-server &
 wait
 
 
 
 echo "Cleaning previous processes...if any..."
-pkill -f wrapper_dashboard_998899.sh &
-wait
+pkill -f wrapper_dashboard_998899.sh
+
 
 echo "Cleaning previous installations..if any..."
 rm -rf serverninja-unix-monitoring &
@@ -80,25 +57,27 @@ echo "Downloading code from repository..."
 git clone https://github.com/omkargudekar/serverninja-unix-monitoring.git &
 wait
 
+echo "Running redis server..."
+nohup redis-server >redis.log &
+
+
+
 
 cd serverninja-unix-monitoring
 
 
 echo "Running server-ninja agent..."
 cd scripts
-nohup sh runagent.sh &
+nohup sh runagent.sh > agentlog &
 echo $$ > server-ninja_agent_pid
 
 cd ..
 
 
 
-echo "Running redis server..."
-nohup redis-server >redis.log &
 
 
-
-echo "Installing server-ninja dashboard setup..."
+echo "Installing server-ninja dashboard setup... DO NOT EXIT"
 npm install --silent &
 wait
 npm install nodemon -g --silent &
@@ -114,21 +93,7 @@ echo $$ > nodeserver_pid
 
 
 
-#publicIP= `wget http://ipinfo.io/ip -qO -`
-#dashboardURL=$publicIP':5145'
-#
-#if curl --output /dev/null --silent --head --fail "$dashboardURL"
-#then
-#    echo "Dashboard is up and running on "+ $dashboardURL
-#else
-#    setterm -term linux -back black -fore red -clear
-#
-#    echo "Unable to access dashboard on " $dashboardURL "Please make sure port is open in your Cloud Hosting"
-#    setterm -reset
-#fi
-#
-
-echo "Bye Bye.. Installation complete...nJoy :) "
+echo "Press any key to continue...nJoy :) "
 
 
 
